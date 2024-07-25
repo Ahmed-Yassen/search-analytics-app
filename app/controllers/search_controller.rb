114a0f4954c2344fd16b9c67d65fdd5211ed8f00
Ahmed Yassen <ahmed.ibrahim.yassen@gmail.com>
2024-07-25 03:47:48 +0300

    fix an issue when submitting with enter key

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,6 +8,7 @@ class SearchController < ApplicationController
     if params[:enter_key_pressed] == 'true'
       # Save complete query directly if Enter key is pressed
       Query.create!(content: query, user_ip: ip)
+      remove_duplicate_from_redis(query, ip)
     else
       # Otherwise, log the query to Redis
       SearchQueryLogger.perform_async(query, ip) if query.split.size > 1
@@ -15,5 +16,13 @@ class SearchController < ApplicationController
 
     @articles = Article.where('title LIKE ?', "%#{query}%")
     render json: { articles: @articles.map { |article| { title: article.title } } }
+  end
+
+  private
+
+  def remove_duplicate_from_redis(query, ip)
+    redis = Redis.new(url: ENV.fetch('REDIS_URL'))
+    key = "#{ip}-#{query.split.first}"
+    redis.del(key)
   end
 end
